a5c18813011cf5a47e1f8c7b7381b32940ff4af1
wanly <36926016+wanlyg@users.noreply.github.com>
2022-05-29 22:38:26 +0800

    Update HttpHelper.cs

--- smsmanager/HttpHelper.cs
+++ smsmanager/HttpHelper.cs
@@ -26,6 +26,24 @@ namespace smsmanager
             myResponseStream.Close();
             return retString;
         }
+        
+        public static string Post(string url, string context)
+        {
+            ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            var streamWriter = new StreamWriter(request.GetRequestStream());
+            streamWriter.Write(context);
+            streamWriter.Flush();
+            streamWriter.Close();
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.UTF8);
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+            return retString;
+        }
 
         public static string Post(string url, JObject obj)
         {
